54958472c3632ddcf6a2b1f96557aadd4e66a4e3
NEOnurlanbek <kushbakovnurlan447@gmail.com>
2024-06-07 16:11:48 +0900

    feat: meta tags integrations on document private file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,24 @@ export default function Document() {
   console.log("Document Pager Router");
   return (
     <Html lang="en">
-      <Head />
+         <Head>
+        <meta charSet='UTF-8' />
+        <meta name='title' content='Nestar Demo' />
+        <meta name='robots' content='index, follow' />
+        <link rel='icon' type='image/png' href='/img/logo/favicon.svg' />
+
+        {/* SEO */}
+
+        <meta name='keyword' content={"nestar, nestar.uz, devex mern, mern nestjs fullstack"} />
+        <meta
+          name='description'
+          content={
+            "Buy and sell propertie anywhere anytime in South Korea. | " +
+            "Покупайте и продавайте недвижимость в любом месте Южной Кореa в любое время . | " +
+            "대한민국 언제 어디서나 부동산을 사고 팔 수 있습니다."
+          }
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
